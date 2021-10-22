7a2f991d4462f34f9a1f2f2558a863bf4bd3e321
saito.k <k.saito@impl.co.jp>
2021-10-22 16:40:55 +0900

    add 記事詳細取得

--- api/src/models/article.ts
+++ api/src/models/article.ts
@@ -2,8 +2,26 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+/**
+ * 全記事取得
+ * @returns
+ */
 export const getArticleAll = async () => {
   const articles = await prisma.article.findMany();
 
   return articles;
+};
+
+/**
+ * 記事詳細取得
+ * @param id
+ * @returns
+ */
+export const getArticleDetail = async (id: string) => {
+  const article = await prisma.article.findUnique({
+    where: { id: id },
+    rejectOnNotFound: true,
+  });
+
+  return article;
 };
