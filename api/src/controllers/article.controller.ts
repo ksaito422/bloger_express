5eb60fa21a1a61b4390f3714f07355751d0306df
saito.k <k.saito@impl.co.jp>
2021-10-22 16:40:55 +0900

    add 記事詳細取得

--- api/src/controllers/article.controller.ts
+++ api/src/controllers/article.controller.ts
@@ -1,6 +1,7 @@
 import { Response, Request, NextFunction } from 'express';
+import { getArticleAll, getArticleDetail } from 'src/models/article';
+import { verifyIdToken } from 'src/services/verifyIdToken';
 import * as HttpErrorCode from 'src/exceptions/errorCode';
-import { getArticleAll } from 'src/models/article';
 
 /**
  * 全記事取得
@@ -13,6 +14,31 @@ export const index = async (req: Request, res: Response, next: NextFunction) => 
     const articles = await getArticleAll();
 
     res.status(200).json(articles);
+  } catch (e) {
+    next(e);
+  }
+};
+
+/**
+ * 記事詳細取得
+ */
+export const detail = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await verifyIdToken(req, next);
+    const article = await getArticleDetail(req.params.articleId);
+
+    if (!article) {
+      throw new Error(HttpErrorCode.HTTP_NOT_FOUND);
+    }
+
+    res.status(200).json({
+      id: article.id,
+      title: article.title,
+      content: article.content,
+      user: {
+        id: article.userId,
+      },
+    });
   } catch (e) {
     next(e);
   }
